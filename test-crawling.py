85af6e5770a015620bd99128b9555aa848a7588f
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-07-27 19:38:40 +0700

    Update test-crawling.py

--- test-crawling.py
+++ test-crawling.py
@@ -1,10 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-url = requests.get('https://www.pagani.com/')
-response = BeautifulSoup (url.text,"html.parser")
-hasil_Scrapping = response.find("h1", class_="block__title-h1")
-print(hasil_Scrapping)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+url = "https://www.rottentomatoes.com/m/superman_2025/reviews"
+driver.get(url)
 
+# Ambil semua elemen dengan data-qa="review-text"
+title = driver.find_element(By.CSS_SELECTOR, 'a[data-qa="sidebar-media-link"]')
+# reviews = driver.find_elements(By.CSS_SELECTOR, 'rt-text[data-qa="review-text"]')
+print(title.text)
 
+# Loop untuk ambil text dari masing-masing elemen
+# for r in reviews:
+#     print(r.text)
+
+driver.quit()
