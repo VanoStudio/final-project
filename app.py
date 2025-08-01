2a33c282f5cfcaa600ae4490bd2377370ba3401c
VanoStudio <91299723+VanoStudio@users.noreply.github.com>
2025-08-01 23:23:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def search_movies(film):
 # === GET CRITIC + AUDIENCE REVIEWS WITH SCROLL AND WAIT ===
 def get_reviews(movie_url):
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
